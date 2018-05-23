44b4505e447a539b6bb56261079d1dfb02c59327
Irbe Krumina <irbekrm@gmail.com>
2018-05-23 17:36:50 +0100

    backstage passes refactored

--- models/shop.js
+++ models/shop.js
@@ -8,33 +8,16 @@ class Shop {
     const itemsList = this.items;
     const specialItems = { 
       'Aged Brie': item => this.updateBrie(item),
-      'Sulfuras, Hand of Ragnaros': _ => { console.log('Hi'); /* do nothing */ }
+      'Sulfuras, Hand of Ragnaros': _ => { console.log('Hi'); /* do nothing */ },
+      'Backstage passes to a TAFKAL80ETC concert': item => this.updateBackstagePasses(item)
     };
     for (var i = 0; i < itemsList.length; i++) {
       if (itemsList[i].name in specialItems) {
         specialItems[itemsList[i].name](itemsList[i]);
       } else {
-      if (itemsList[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (itemsList[i].quality > 0) {
             itemsList[i].quality = itemsList[i].quality - 1;
         }
-      } else {
-        if (itemsList[i].quality < 50) {
-          itemsList[i].quality = itemsList[i].quality + 1;
-          if (itemsList[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (itemsList[i].sellIn < 11) {
-              if (itemsList[i].quality < 50) {
-                itemsList[i].quality = itemsList[i].quality + 1;
-              }
-            }
-            if (itemsList[i].sellIn < 6) {
-              if (itemsList[i].quality < 50) {
-                itemsList[i].quality = itemsList[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
         itemsList[i].sellIn = itemsList[i].sellIn - 1;
       if (itemsList[i].sellIn < 0) {
           if (itemsList[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -55,6 +38,17 @@ class Shop {
     item.sellIn--;
   }
 
+  updateBackstagePasses(item) {
+    if(item.sellIn <=0) { this.updateItem(item, -item.quality); return; }
+    if(item.sellIn <= 5) { this.updateItem(item, 3); return; }
+    if(item.sellIn <= 10) { this.updateItem(item, 2); return; }
+    this.updateItem(item, 1);
+  }
+  
+  updateItem(item, qual=0) {
+    item.sellIn--;
+    item.quality = Math.min(item.quality += qual, 50);
+  }    
   
 }
 module.exports = Shop;
