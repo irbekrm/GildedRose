1ad0345e9bdefe44738e05edfd18fc6a19cae567
Irbe Krumina <irbekrm@gmail.com>
2018-05-24 06:14:27 +0100

    remove unnecessary lines

--- test/updaters/conjuredTest.js
+++ test/updaters/conjuredTest.js
@@ -2,11 +2,10 @@
 ({ conjuredUpdate } = require('../../models/updaters/conjuredUpdater'));
 
 const conjured = { name: 'Conjured', quality: 21, sellIn: 3 },
-  lateConjured = { name: 'Conjured', quality: 22, sellIn: -2 },
-  smallConjured = { name: 'Conjured', quality: 49, sellIn: 20 };
+  lateConjured = { name: 'Conjured', quality: 22, sellIn: -2 };
 
 describe('conjuredItems decrease in quality twice as fast', _ => {
-  it('decreases in quality by 2 before sellIn date', done => {
+  it('decreases in quality by 2 before sellIn days', done => {
     conjuredUpdate(conjured);
     expect(conjured.quality).to.equal(19);
   done();
