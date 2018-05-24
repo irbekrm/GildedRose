12bd2565085990dfaa5839e5fbb19280f0c7c64a
Irbe Krumina <irbekrm@gmail.com>
2018-05-24 10:06:54 +0100

    genericItem class instance creation- tested

---
+++ test/genericItemTest.js
@@ -0,0 +1,17 @@
+({ expect } = require('chai').expect);
+const Item = require('../models/item'),
+  GenericItem = require('../models/genericItem');
+
+var genericItem;
+
+before(done => {
+  genericItem = new GenericItem('juice', 20, 30);
+}); 
+
+describe('new generic item', _ => {
+  it('has a name', done => {
+    expect(genericItem.name).to.equal('juice');
+    expect(genericItem.sellIn).to.equal(20);
+    expect(genericItem.quality).to.equal(30);
+  });
+});
