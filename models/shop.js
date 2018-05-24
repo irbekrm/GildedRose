4e6d95d2fe2085d6f33d9c32c27cc0e60dc1bd91
Irbe Krumina <irbekrm@gmail.com>
2018-05-24 14:52:42 +0100

    shop tests updated

--- models/shop.js
+++ models/shop.js
@@ -1,21 +1,10 @@
-({ brieUpdate } = require('./updaters/brieUpdater'));
-({ backstagePassesUpdate } = require('./updaters/backstagePassesUpdater'));
-({ genericUpdate } = require('./updaters/genericUpdater'));
-
-const Item = require('./item.js'),
-  specialItems = { 
-    'Aged Brie': item => brieUpdate(item),
-    'Sulfuras, Hand of Ragnaros': _ => { /* do nothing */ },
-    'Backstage passes to a TAFKAL80ETC concert': item =>  backstagePassesUpdate(item)
-  };
-
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
   updateQuality() {
     this.items.forEach(item => {
-      (specialItems[item.name] || genericUpdate)(item)
+      item.updateQuality();
     });
   return this.items;
   } 
