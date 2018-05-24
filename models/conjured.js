f347cb66e5e6758d45fbf980d8847acf737a9bc9
Irbe Krumina <irbekrm@gmail.com>
2018-05-24 14:29:24 +0100

    conjured updated - implemented

--- models/conjured.js
+++ models/conjured.js
@@ -4,6 +4,12 @@ class Conjured extends GenericItem {
   constructor(name, sellIn, quality) {
     super(name, sellIn, quality);
   }
+
+  updateQuality() {
+    this.sellIn > 0 ?
+    this.setState(-2) :
+    this.setState(-4)
+  }
 }
 
 module.exports = Conjured;
