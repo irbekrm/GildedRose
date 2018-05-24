9ffd575a76d0861b31f1979c91c730c97d3a7af5
Irbe Krumina <irbekrm@gmail.com>
2018-05-24 10:26:41 +0100

    updateState for genericItem -tested

--- test/genericItemTest.js
+++ test/genericItemTest.js
@@ -2,10 +2,14 @@
 const Item = require('../models/item'),
   GenericItem = require('../models/genericItem');
 
-var genericItem;
+var genericItem,
+    bigItem,
+    littleItem;
 
-before(done => {
+beforeEach(done => {
   genericItem = new GenericItem('juice', 20, 30);
+  bigItem = new GenericItem('spinach', 10, 49);
+  littleItem = new GenericItem('eggs', 3, 1);
   done();
 }); 
 
@@ -17,3 +21,24 @@ describe('new generic item', _ => {
   done();
   });
 });
+
+describe('updating state of an item', _ => {
+  it('decreases the sellIn by 1', done => {
+    genericItem.updateState(3);
+    expect(genericItem.sellIn).to.equal(29);
+  done();
+  });
+
+  it('does not allow the quality to exceed 50', done => {
+    bigItem.updateState(2);
+    expect(bigItem.quality).to.equal(50);
+  done();
+  });
+
+  it('does not allow the quality to fall below 0', done => {
+    littleItem.updateState(-3);
+    expect(littleItem.quality).to.equal(0);
+  done();
+  });
+}); 
+
