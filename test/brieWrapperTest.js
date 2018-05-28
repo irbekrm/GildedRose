ec09c348314cbc46bdb2d6197a1a990e2f232195
Irbe Krumina <irbekrm@gmail.com>
2018-05-28 01:23:33 +0100

    refactored and renamed brie -> brieWrapper, tests adjusted

--- test/brieWrapperTest.js
+++ test/brieWrapperTest.js
@@ -1,19 +1,15 @@
 const expect = require('chai').expect,
-  Brie = require('../models/brie');
+  Item = require('../models/item'),
+  BrieWrapper = require('../models/brieWrapper');
 
 var brie;
 
 beforeEach(done => {
-  brie = new Brie('Aged Brie', 4, 5);
+  brie = new BrieWrapper(new Item('Aged Brie', 4, 5));
   done();
 });
 
 describe('creating a new brie', _ => {
-  it('has a name', done => {
-    expect(brie.name).to.equal('Aged Brie');
-  done();
-  });
-
   it('has a sellIn', done => {
     expect(brie.sellIn).to.equal(4);
   done();
