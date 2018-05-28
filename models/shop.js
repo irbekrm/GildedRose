120d1b0d80f328a504491e3f5eaf25ac551f46ef
Irbe Krumina <irbekrm@gmail.com>
2018-05-28 07:32:07 +0100

    shop refactored

--- models/shop.js
+++ models/shop.js
@@ -1,10 +1,25 @@
+const BrieWrapper = require('./brieWrapper'),
+  ConjuredWrapper = require('./conjuredWrapper'),
+  SulfurasWrapper = require('./sulfurasWrapper'),
+  BackstagePassWrapper = require('./backstagePassWrapper'),
+  GenericWrapper = require('./genericWrapper'),
+  specialItemNames = {
+    'Aged Brie': BrieWrapper,
+    'Sulfuras, Hand of Ragnaros': SulfurasWrapper,
+    'Backstage passes to a TAFKAL80ETC concert': BackstagePassWrapper,
+    'Conjured Mana Cake': ConjuredWrapper
+ }; 
+    
+  
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
   updateQuality() {
     this.items.forEach(item => {
-      item.updateQuality();
+      var itemClass = specialItemNames[item.name] || GenericWrapper;
+      console.log('item class is ', itemClass);
+      (new itemClass(item)).updateQuality();
     });
   return this.items;
   } 
