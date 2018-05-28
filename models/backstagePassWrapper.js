6e2c78af8a164fc6b554da225c2ae591c92c94a0
Irbe Krumina <irbekrm@gmail.com>
2018-05-28 01:11:43 +0100

    refactor and rename backstagePass > backstagePassWrapper, adjust tests

---
+++ models/backstagePassWrapper.js
@@ -0,0 +1,19 @@
+const GenericWrapper = require('./genericWrapper');
+
+class BackstagePassWrapper extends GenericWrapper {
+  constructor(item){
+    super(item);
+  }
+  
+  updateQuality() {
+    this.sellIn <= 0 ?
+    this.setState(-this.quality) :
+    this.sellIn <= 5 ?
+    this.setState(3) :
+    this.sellIn <= 10 ?
+    this.setState(2) :
+    this.setState(1)
+  }
+}
+
+module.exports = BackstagePassWrapper;
